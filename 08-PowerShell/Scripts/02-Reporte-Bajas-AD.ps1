60c2c9434b68b770e5007222277376fb45cd861f
NetBr#1 <braianffernandez096@gmail.com>
2026-08-13 17:30:29 -0300

    Update 02-Reporte-Bajas-AD.ps1

--- 08-PowerShell/Scripts/02-Reporte-Bajas-AD.ps1
+++ 08-PowerShell/Scripts/02-Reporte-Bajas-AD.ps1
@@ -1,1 +1,10 @@
+Import-Module ActiveDirectory
 
+$reporte = Get-ADUser -Filter {Enabled -eq $false} `
+    -SearchBase "OU=TechSolutions,DC=techsolutions,DC=local" `
+    -Properties Description, whenChanged |
+    Select-Object Name, SamAccountName, Description, whenChanged |
+    Sort-Object whenChanged -Descending
+
+$reporte | Format-Table -AutoSize
+$reporte | Export-Csv -Path "C:\Scripts\Reporte_Bajas.csv" -NoTypeInformation -Encoding UTF8
